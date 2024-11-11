5de5f98ae41bd81651ab7f60aa6c291cb2220d1f
Abel <you@example.com>
2024-11-11 22:28:38 +0300

    First commit.

---
+++ postcss.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: { // This is to enable TailwindCSS to work with Next.js
+        tailwindcss: {},
+    },
+}
+
+export default config
