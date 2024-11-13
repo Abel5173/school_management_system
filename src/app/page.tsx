036167af279e934e6928d8ada16a659646be6b6d
Abel <you@example.com>
2024-11-13 22:23:52 +0300

    Navbar and Menu done.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Homepage = () => {
     return(
         <div className="">
-            <h1>Homepage</h1>
+            <h1 className="text-3xl text-center m-2 bg-emerald-600 text-gray-50 py-4 rounded-lg">Homepage</h1>
             <p>Welcome to the homepage</p>
         </div>
     )
